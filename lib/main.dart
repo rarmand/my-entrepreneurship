a0f2242e6e7b13b3736cd512df2251643737f627
rarmand <holik.alek@gmail.com>
2024-03-09 00:49:21 +0100

    firebase initialized in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:my_entrepreneurship/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  
   runApp(const FinancesApp());
 }
