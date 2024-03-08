dc82462b42fa4def7d8ac7818a444a7a6f7070ee
rarmand <holik.alek@gmail.com>
2024-03-08 13:48:02 +0100

    prepared simple bloc structure

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_entrepreneurship/ui/pages/main_page.dart';
+
+class FinancesApp extends MaterialApp {
+  const FinancesApp({super.key}): super(home: const MainPage());
+}
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MainView extends StatelessWidget {
-  const MainView({super.key});
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.appTitle)),
+      appBar: AppBar(title: const Text("App title")),
       body: const Center(child: Text('Starting view right after loggin in or entering the app'),)
     );
   } 
