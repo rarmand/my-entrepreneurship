13092db6f47131416eeaf99c9e679c09d6af8248
rarmand <holik.alek@gmail.com>
2024-03-11 14:55:48 +0100

    corrected l10n folder

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_entrepreneurship/home/home.dart';
+import 'package:my_entrepreneurship/l10n/l10n.dart';
 
 class App extends MaterialApp {
-  const App({super.key}): super(home: const HomePage());
+  const App({super.key}): super(
+      title: 'Portfello',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const HomePage()
+    );
 }
