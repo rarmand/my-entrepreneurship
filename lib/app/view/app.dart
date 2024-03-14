f3ce6fcfcdd27c85b3fae78c56714a798dc806a3
rarmand <holik.alek@gmail.com>
2024-03-14 15:17:14 +0100

    added routing to App

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -3,8 +3,12 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_entrepreneurship/home/home.dart';
 import 'package:my_entrepreneurship/l10n/l10n.dart';
 
-class App extends MaterialApp {
-  const App({super.key}): super(
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Portfello',
       localizationsDelegates: const [
         AppLocalizations.delegate,
@@ -13,6 +17,11 @@ class App extends MaterialApp {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const HomePage()
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => const HomePage(),
+        '/home': (context) => const HomePage(),
+      },
     );
+  }
 }
