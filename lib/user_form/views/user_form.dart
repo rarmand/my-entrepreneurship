87b97831fc6eeae7a9dcbedcdc7e7e63b267edbe
rarmand <holik.alek@gmail.com>
2024-03-15 15:10:16 +0100

    user_form divided to 2 views

--- lib/user_form/views/user_form.dart
+++ lib/user_form/views/user_form.dart
@@ -27,87 +27,122 @@ class _UserFormState extends State<UserForm> {
 
   @override
   Widget build(BuildContext context) {
+
     return Form(
       key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormUsernameLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormUsernameValidationLabel;
-              }
-              return null;
-            },
-          ),
+      child: Scaffold(
+        body: PageView(
+          children: [
+            Container(
+              // page 1
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormUsernameLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormUsernameValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
 
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormBirthdayYearLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormBirthdayYearValidationLabel;
-              }
-              return null;
-            },
-          ),
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormBirthdayYearLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormBirthdayYearValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
 
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormAvgMonthlyIncomeLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormAvgMonthlyIncomeValidationLabel;
-              }
-              return null;
-            },
-          ),
-
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormIncomeCurrencyLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormIncomeCurrencyValidationLabel;
-              }
-              return null;
-            },
-          ),
-
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormIncomeDateLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormIncomeDateValidationLabel;
-              }
-              return null;
-            },
-          ),
-
-          CustomFormField(
-            labelText: AppLocalizations.of(context).userFormFreeAmountLabel,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context).userFormFreeAmountValidationLabel;
-              }
-              return null;
-            },
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8), 
-            child: ElevatedButton(
-              child: Text(AppLocalizations.of(context).userFormSubmitButton),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(AppLocalizations.of(context).userFormProcessingInfo)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8), 
+                    child: ElevatedButton(
+                      child: Text(AppLocalizations.of(context).userFormSubmitButton),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(AppLocalizations.of(context).userFormProcessingInfo)
+                            )
+                          );
+                        }
+                      },
                     )
-                  );
-                }
-              },
-            )
-          ),
-        ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              // page 2
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormAvgMonthlyIncomeLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormAvgMonthlyIncomeValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
+
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormIncomeCurrencyLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormIncomeCurrencyValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
+
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormIncomeDateLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormIncomeDateValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
+
+                  CustomFormField(
+                    labelText: AppLocalizations.of(context).userFormFreeAmountLabel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return AppLocalizations.of(context).userFormFreeAmountValidationLabel;
+                      }
+                      return null;
+                    },
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8), 
+                    child: ElevatedButton(
+                      child: Text(AppLocalizations.of(context).userFormSubmitButton),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(AppLocalizations.of(context).userFormProcessingInfo)
+                            )
+                          );
+                        }
+                      },
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
