d423b464388a00e5f868a9458b39d064044c5f67
rarmand <holik.alek@gmail.com>
2024-03-19 01:34:44 +0100

    User repository detailed

--- packages/user_repository/lib/src/models/user.dart
+++ packages/user_repository/lib/src/models/user.dart
@@ -1,12 +1,27 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  const User(this.id);
+  User(this.id, {
+    this.username = '',
+    this.birthYear = -1,
+    this.avgMonthlyIncome = -0.1,
+    this.incomeCurrency = '',
+    DateTime? incomeDate,
+    this.freeAmount = -1.0,
+  }): this.incomeDate = incomeDate ?? DateTime.now();
 
   final String id;
+  final String username;
+  final int birthYear;
+  final double avgMonthlyIncome;
+  final String incomeCurrency;
+  final DateTime incomeDate;
+  final double freeAmount;
 
   @override
-  List<Object> get props => [id];
+  List<Object> get props => [
+    id, username, birthYear, avgMonthlyIncome, incomeCurrency, incomeDate, freeAmount
+  ];
 
-  static const empty = User('-');
+  static User empty = User('-');
 }
