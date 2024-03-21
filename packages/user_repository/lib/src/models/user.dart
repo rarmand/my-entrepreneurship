8fcf1f45d12053f1cbd78225193498422954b933
rarmand <holik.alek@gmail.com>
2024-03-21 01:14:00 +0100

    added documentation to user repository user

--- packages/user_repository/lib/src/models/user.dart
+++ packages/user_repository/lib/src/models/user.dart
@@ -1,27 +1,49 @@
 import 'package:equatable/equatable.dart';
 
+/// {@template user}
+/// User model
+/// {@endtemplate}
 class User extends Equatable {
-  User(this.id, {
-    this.username = '',
-    this.birthYear = -1,
-    this.avgMonthlyIncome = -0.1,
-    this.incomeCurrency = '',
+  /// {@macro user}
+  User({
+    required this.id,
+    this.username,
+    this.birthYear,
+    this.avgMonthlyIncome,
+    this.incomeCurrency,
     DateTime? incomeDate,
-    this.freeAmount = -1.0,
+    this.freeAmount,
+    this.isNewUser = true,
   }): this.incomeDate = incomeDate ?? DateTime.now();
 
+  /// The current user's id
   final String id;
-  final String username;
-  final int birthYear;
-  final double avgMonthlyIncome;
-  final String incomeCurrency;
-  final DateTime incomeDate;
-  final double freeAmount;
+
+  /// The current user's username (display name)
+  final String? username;
+
+  /// The current user's birth year for calculations
+  final int? birthYear;
+
+  /// The current user's average monthly income for calculations
+  final double? avgMonthlyIncome;
+
+  /// The current user's income currency for calculations
+  final String? incomeCurrency;
+  
+  /// Date of registered income of the current user for calculations
+  final DateTime? incomeDate;
+  
+  /// The current user's free money amount for calculations
+  final double? freeAmount;
+
+  /// Whether the current user is a first time user
+  final bool isNewUser;
+
+  // static const initial = User(id: '');
 
   @override
-  List<Object> get props => [
-    id, username, birthYear, avgMonthlyIncome, incomeCurrency, incomeDate, freeAmount
+  List<Object?> get props => [
+    id, username, birthYear, avgMonthlyIncome, incomeCurrency, incomeDate, freeAmount, isNewUser,
   ];
-
-  static User empty = User('-');
 }
