95dd8e804feff1715801900edca71bee29bad443
rarmand <holik.alek@gmail.com>
2024-03-22 02:31:33 +0100

    updated custom text field

--- lib/user_form/widgets/custom_text_form_field.dart
+++ lib/user_form/widgets/custom_text_form_field.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class CustomTextFormField extends StatelessWidget {
   const CustomTextFormField({
     super.key,
+    required this.name,
     required this.labelText,
-    required this.onChanged,
+    this.valueTransformer,
     this.keyboardType = TextInputType.text,
     this.maxLength = 20,
     this.inputFormatters,
     this.validator,
   });
 
+  final String name;
   final String labelText;
+
   final int? maxLength;
   final TextInputType keyboardType;
   final List<TextInputFormatter>? inputFormatters;
+
   final String? Function(String?)? validator;
-  final void Function(String?) onChanged; 
+  final dynamic Function(String?)? valueTransformer; 
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4), 
-      child: TextFormField(
-        onChanged: onChanged,
+      child: FormBuilderTextField(
+        name: name,
+        valueTransformer: valueTransformer,
         keyboardType: keyboardType,
         inputFormatters: inputFormatters,
         validator: validator,
