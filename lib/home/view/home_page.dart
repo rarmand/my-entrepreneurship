aefe5d62bbc474b889f93454ff62070b416e7e92
rarmand <holik.alek@gmail.com>
2024-03-24 01:56:20 +0100

    added menu prototype

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:my_entrepreneurship/l10n/l10n.dart';
-import 'package:my_entrepreneurship/user_form/views/views.dart';
+import 'package:my_entrepreneurship/user_profile/user_profile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,7 +11,31 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context).appTitle)),
-      body: const Center(child: UserForm()),
+      body: const Center(child: UserProfilePage()),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: Colors.black12,
+        color: Colors.black12,
+        animationDuration: const Duration(microseconds: 300),
+        items: const <CurvedNavigationBarItem>[
+          CurvedNavigationBarItem(
+            child: Icon(Icons.home, size: 26, color: Colors.grey),
+            label: 'Home',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.business_center, size: 26, color: Colors.grey),
+            label: 'Savings',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.payment, size: 26, color: Colors.grey),
+            label: 'Budget',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.account_box, size: 26, color: Colors.grey),
+            label: 'User',
+          ),
+        ],
+      ),
     );
   } 
 }
