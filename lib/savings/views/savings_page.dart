59431c459d62d7da86b8f3806fa81c6804f023f3
rarmand <holik.alek@gmail.com>
2024-04-02 22:44:40 +0200

    added savings page

---
+++ lib/savings/views/savings_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SavingsPage extends StatelessWidget {
+  const SavingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Savings page")),
+    );
+  } 
+}
---
+++ lib/savings/views/views.dart
@@ -0,0 +1,1 @@
+export 'savings_page.dart';
