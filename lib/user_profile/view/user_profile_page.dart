3a597bb4abdcd78723932d7d345b87adc24ae0a2
rarmand <holik.alek@gmail.com>
2024-04-12 01:16:28 +0200

    changes in user profile

--- lib/user_profile/view/user_profile_page.dart
+++ lib/user_profile/view/user_profile_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:my_entrepreneurship/l10n/l10n.dart';
 
-import 'user_profile.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool isViewDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
     return Scaffold(
-      body: const Center(child: UserProfile()),
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context).userProfileTitle,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // TODO: change theme set
+            }, 
+            icon: Icon(isViewDark ? Icons.light_mode : Icons.dark_mode),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+
+        ),
+      ),
     );
   } 
 }
