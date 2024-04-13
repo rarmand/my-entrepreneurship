714bc4c559aaa04ab18c36456f1d63c169b1160b
rarmand <holik.alek@gmail.com>
2024-04-13 15:46:29 +0200

    big stuff added to user profile view - prototype

--- lib/user_profile/view/user_profile_page.dart
+++ lib/user_profile/view/user_profile_page.dart
@@ -11,22 +11,122 @@ class UserProfilePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           AppLocalizations.of(context).userProfileTitle,
           style: Theme.of(context).textTheme.titleLarge,
         ),
         actions: [
           IconButton(
+            icon: Icon(isViewDark ? Icons.light_mode : Icons.dark_mode),
+            tooltip: "Change view mode",
             onPressed: () {
+              // TODO: change view mode
               // TODO: change theme set
             }, 
-            icon: Icon(isViewDark ? Icons.light_mode : Icons.dark_mode),
+          ),
+          IconButton(
+            icon: const Icon(Icons.edit),
+            tooltip: "Edit profile",
+            onPressed: () {
+              // TODO: navigate to edition profile view
+              // TODO: change theme set
+            }, 
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 100.0,
+                  backgroundColor: Colors.orange,
+                ),
+                const SizedBox(height: 10.0),
 
+                Column(
+                  children: [
+                    TextFormField(
+                      initialValue: "James Anderson",
+                      readOnly: true,
+                      enableSuggestions: false,
+                      decoration: const InputDecoration(
+                        helperText: "Username",
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                      
+
+                    TextFormField(
+                      initialValue: "1999",
+                      readOnly: true,
+                      enableSuggestions: false,
+                      decoration: const InputDecoration(
+                        helperText: "Birth year",
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+
+                    TextFormField(
+                      initialValue: "8000 PLN",
+                      readOnly: true,
+                      enableSuggestions: false,
+                      decoration: const InputDecoration(
+                        helperText: "Average income",
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+
+                    TextFormField(
+                      initialValue: "2000 PLN",
+                      readOnly: true,
+                      enableSuggestions: false,
+                      decoration: const InputDecoration(
+                        helperText: "Free amount",
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],   
+                ),
+              ], 
+            ),
+          ),
         ),
       ),
     );
