02f0cc5f09aee173666495a476b1bcd90d7bc747
rarmand <holik.alek@gmail.com>
2024-04-21 15:12:27 +0200

    tested more combined savings version

--- lib/savings/views/savings_page.dart
+++ lib/savings/views/savings_page.dart
@@ -6,36 +6,45 @@ class SavingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    PieChartData data = PieChartData(
-      centerSpaceRadius: 24.0,
-      sectionsSpace: 1,
-      sections: <PieChartSectionData>[
-        PieChartSectionData(
-          title: 'Obligacje skarbowe',
-          value: 10000,
-          color: Colors.yellow,
-        ),
-        PieChartSectionData(
-          title: 'Lokata',
-          value: 30000,
-          color: Colors.green,
-        ),
-        PieChartSectionData(
-          title: 'Kryptowaluty',
-          value: 5000,
-          color: Colors.blue,
-        ),
-        PieChartSectionData(
-          title: 'Akcje giełdowe',
-          value: 6000,
-          color: Colors.red,
-        ),
-      ],
+    int touchedIndex = 0;
+
+    // TODO: do pobrania z bazy danych
+    const double freeCapital = 2000.0;
+    const String currency = 'PLN';
+
+    // TODO: do pobrania z bazy danych
+    // do połączenia z klasą investment z constants
+    final data = [
+      {'name': 'obligacje', 'value': 10000.0, 'color': Colors.cyan},
+      {'name': 'lokaty', 'value': 30000.0, 'color': Colors.deepOrange},
+      {'name': 'akcje', 'value': 5000.0, 'color': Colors.blueGrey},
+      {'name': 'kryptowaluty', 'value': 15000.0, 'color': Colors.lightGreen},
+    ];
+
+    List<PieChartSectionData> generateSections() {
+      return data.map((obj) {
+        return PieChartSectionData(
+          title: obj['name'].toString().toUpperCase(),
+          titleStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+          color: obj['color'] as Color,
+          value: obj['value'] as double,
+        );
+      }).toList();
+    }
+
+    PieChartData pieChartData = PieChartData(
+      startDegreeOffset: 0,
+      centerSpaceRadius: double.infinity,
+      sectionsSpace: 4.0,
+      sections: generateSections(),
     );
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Savings'),
+        centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
@@ -47,20 +56,44 @@ class SavingsPage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: AspectRatio(
-          aspectRatio: 16/9,
+        child: Container(
+          alignment: Alignment.topCenter,
           child: Column(
             children: [
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: PieChart(
-                    data,
-                    swapAnimationDuration: const Duration(milliseconds: 150),
-                    swapAnimationCurve: Curves.linear,
-                  ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0), 
+                child: Column(
+                  children: [
+                    Text(
+                      'Free capital',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      '$freeCapital $currency',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w700,
+                      ), 
+                    ),
+                  ],
                 ),
               ),
+              
+              const SizedBox(height: 24.0),
+
+              SizedBox(
+                width: 300,
+                height: 300,
+                child: PieChart(
+                  pieChartData,
+                  swapAnimationDuration: const Duration(milliseconds: 150),
+                  swapAnimationCurve: Curves.linear,
+                ),
+              ),
+              
               const SizedBox(height: 24.0),
               
               // TODO: list of indicators?
